40d12f7541836d5ddf0e48fd8ea320872617a4ef
rohitsurage <rohit.surage@codiotic.com>
2024-02-14 15:51:37 +0530

    final-code

---
+++ lib/app/utils/project_utils.dart
@@ -0,0 +1,66 @@
+class ProjectUtils {
+  final String banners;
+  final String icons;
+  final String titles;
+  final String description;
+  final String links;
+  ProjectUtils({
+    required this.banners,
+    required this.icons,
+    required this.titles,
+    required this.description,
+    required this.links,
+  });
+}
+
+List<ProjectUtils> projectUtils = [
+  ProjectUtils(
+    banners: 'assets/imgs/02.png',
+    icons: 'assets/imgs/flutter.png',
+    titles: 'Random Number On Dice And Counts',
+    description: "A simple application designed to simulate the rolling of dice and keep track of the count for each rolled number. Utilizing random number generation techniques, the app provides a user-friendly interface where users can roll virtual dice, observe the outcome, and see the cumulative count for each number rolled. This project serves as an engaging tool for users to explore random number generation concepts and practice basic counting skills in a fun and interactive way.",
+    links: 'https://github.com/RohitSurage99/Random_number_dice_and_counter',
+  ),
+  ProjectUtils(
+    banners: 'assets/imgs/1.png',
+    icons: 'assets/imgs/flutter.png',
+    titles: 'Awesome Calculator',
+    description: 'The Flutter Calculator is a mobile application developed using the Flutter framework, designed to provide users with a convenient tool for performing basic arithmetic operations. With a user-friendly interface and smooth functionality, it allows users to input numbers and operators easily, displaying results instantly. Leveraging the power of Flutter, the calculator offers cross-platform compatibility, ensuring seamless performance on both Android and iOS devices.',
+    links: 'https://github.com/RohitSurage99/Flutter_Calculator',
+  ),
+  ProjectUtils(
+    banners: 'assets/imgs/03.png',
+    icons: 'assets/imgs/flutter.png',
+    titles: 'Cocktail Drinks App',
+    description: 'A comprehensive collection of cocktail recipes and drink ideas. With an intuitive interface, users can browse through a wide variety of cocktails categorized by type, ingredients, or popularity. Each recipe includes detailed instructions, ingredient lists, and serving suggestions.',
+    links: 'https://github.com/RohitSurage99/Cocktail_drinks_app',
+  ),
+  ProjectUtils(
+    banners: 'assets/imgs/04.png',
+    icons: 'assets/imgs/flutter.png',
+    titles: 'Flutter Chat GPT Clone',
+    description: 'The Flutter ChatGPT Clone is a conversational AI application developed using the Flutter framework. Leveraging the power of OpenAIs GPT models, the clone enables users to engage in text-based conversations with an artificial intelligence agent. With a sleek and intuitive user interface built using Flutter widgets, the app provides a seamless experience for users to interact with the AI, whether it is for seeking information, generating text, or simply chatting.',
+    links: 'https://github.com/RohitSurage99/chat_gpt_clone',
+  ),
+  ProjectUtils(
+    banners: 'assets/imgs/05.png',
+    icons: 'assets/imgs/flutter.png',
+    titles: 'City Bazaar Online App',
+    description: 'The City Bazaar online shopping app, developed using Flutter, offers users a convenient platform for both online and offline shopping experiences.',
+     links: 'https://play.google.com/store/apps/details?id=com.citybazaronline&hl=en_IN&gl=US',
+  ),
+  ProjectUtils(
+    banners: 'assets/imgs/06.png',
+    icons: 'assets/imgs/flutter.png',
+    titles: 'Codiotic Connect App',
+    description: 'we develop "Codiotic Connect App" this is our internal app for client.',
+    links: '',
+  ),
+  ProjectUtils(
+    banners: 'assets/imgs/07.png',
+    icons: 'assets/imgs/flutter.png',
+    titles: 'To Be Continue For More...',
+    description: '',
+    links: '',
+  ),
+];
---
+++ lib/changes/links.dart
@@ -0,0 +1,29 @@
+// CV
+const String resume = "";
+    // 'https://drive.google.com/file/d/1bDNNrkQtNVJbXcdwMSx0crQQvAF_nMwV/view?usp=sharing';
+
+// Github Link
+const String gitHub = 'https://github.com/RohitSurage99';
+// Github Link
+const String facebook = 'https://www.facebook.com/rohitsurage101';
+// Github Link
+const String twitter = 'https://twitter.com/ErSurage';
+// Github Link
+const String youtube =
+    'https://www.youtube.com/channel/UCiX3wgTpUrT5a65NaFB8IKQ';
+// Github Link
+const String linkedin = 'https://www.linkedin.com/in/rohit-surage-85b2271b1';
+// Github Link
+const String buymeacoffee = 'https://www.buymeacoffee.com';
+// Github Link
+const String instagram = 'https://www.instagram.com/rohitsurage101';
+
+const String profileImage =
+    "https://images.unsplash.com/photo-1503443207922-dff7d543fd0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80";
+//
+// Github Link
+const String fiverr = 'https://www.fiverr.com';
+// Github Link
+const String upwork = 'https://www.upwork.com';
+// Github Link
+const String whatsapp = 'https://api.whatsapp.com/send?phone=9589631694';
