6dea815544c0f0028be1e39ad77cda39a51486fb
Rohit Surage <67405070+RohitSurage99@users.noreply.github.com>
2024-12-22 14:04:58 +0530

    Update strings.dart
    strings changes

--- lib/changes/strings.dart
+++ lib/changes/strings.dart
@@ -12,10 +12,12 @@ String contactHeadding = "Let’s try my service now!";
 String contactSubHeadding =
     "Let’s work together and make everything super cute and super useful.";
 //
-String miniDescription = "Freelancer providing services for programming and design content needs. Join me down below and let's get started!";
+String miniDescription =
+    'Hi 👋 I am a Senior Flutter Developer with 4+ years of experience, delivering 20+ high-quality apps across industries. Skilled in Flutter, Dart, API integration, app optimization, and monetization strategies. I have collaborated with clients globally and led teams to deliver top-notch solutions on time.';
+// "Freelancer providing services for programming and design content needs. Join me down below and let's get started!";
 
 String servicesSubHeading =
-    "Since the beginning of my journey as a freelance designer and developer, I've worked in startups and collaborated with talented people to create digital products for both business and consumer use. I offer a wide range of services, including brand design, programming and teaching.";
+    "Since the beginning of my journey as a developer, I've worked in startups and collaborated with talented people to create digital products for both business and consumer use. I offer a wide range of services, including brand design, programming and teaching.";
 //
 
 String protfolioSubHeading =
@@ -24,4 +26,4 @@ const String aboutMeHeadline =
     'I\'m Rohit Surage, a Mobile App developer, Web developer and UI designer.';
 
 const String aboutMeDetail =
-    'I\'m a Fresh Graduate as Software Engineer from SVCE Indore, INDIA. I have been developing mobile apps for over 3 years now. I have worked in teams for various startups and helped them in launching their prototypes and got valuable learning experience. Currently working as an Associate Software Engineer at CODIOTIC Technology PVT. LTD.';
+    'I\'m a Fresh Graduate as Software Engineer from SVCE Indore, INDIA. I have been developing mobile apps for over 4 years now. I have worked in teams for various startups and helped them in launching their prototypes and got valuable learning experience. Currently working as an Associate Software Engineer at CODIOTIC Technology PVT. LTD.';
